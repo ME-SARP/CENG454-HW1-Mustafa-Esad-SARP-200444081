b483dfe1ea625b5277c414e4b797a91fa8f51488
Mustafa Esad SARP <s200444081@stu.thk.edu.tr>
2026-03-22 17:50:15 +0300

    feat: setup flight controller skeleton and initialize rigidbody

---
+++ flight simulator/Assets/scripts/FlightController.cs
@@ -0,0 +1,28 @@
+// FlightController.cs
+// CENG 454 - HW1: Sky-High Prototype
+// Author: Mustafa Esad Sarp | Student ID: 200444081
+
+using UnityEngine;
+
+public class FlightController : MonoBehaviour
+{
+    [SerializeField] private float pitchSpeed = -20f;   
+    [SerializeField] private float yawSpeed = 15f;      
+    [SerializeField] private float rollSpeed = 45f;     
+    [SerializeField] private float thrustSpeed = 100f;  
+
+    
+    private Rigidbody rb;
+
+    void Start()
+    {
+        
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+
+    void Update()
+    {
+        
+    }
+}
